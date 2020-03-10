98d5a628557e1a7d7a12c7df57a2cf5b85df0dc0
Leena <ihanokvitsiniekka@gmail.com>
2020-03-10 10:48:39 +0200

    added nav-bar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import './App.css';
 
+import NavBar from "./Components/NavBar";
 import Fruits from "./Components/Fruits";
 import Cars from "./Components/Cars";
+
+const Home = (props) => <div>Tämä on koti</div>
 
 const cars = [
   {"id": 1, "make": "ford", "model": "Mustang"},
@@ -12,11 +16,21 @@ const cars = [
   {"id": 5, "make": "skoda", "model": "Fabia"},
   {"id": 6, "make": "ford", "model": "T"},
   {"id": 7, "make": "volvo", "model": "V40"}
-] 
+];
 
 function App() {
+  return (
+    <Router>
+      <NavBar />
+    <Switch>
+    <Route exact path="/" component={Home}/>
+    <Route path="/cars"><Cars listOfCars={cars}/></Route>
+    <Route render={() => "404 - Not Found!"}/>
+    </Switch>
+    </Router>
+    );
   
-  return ( <Fruits listOfFruits={fruits} />
+  return ( <Cars listOfCars={cars} />
   );
 }
 
