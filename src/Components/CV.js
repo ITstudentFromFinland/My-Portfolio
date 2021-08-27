2c7fd3b60aa2d355b5b798a05bc9b1c40894e0af
Leena Palsio <leena@outlook.com>
2021-08-27 23:53:13 +0300

    modified CV page

--- src/Components/CV.js
+++ src/Components/CV.js
@@ -1,8 +1,17 @@
 import React from "react";
 
+import "../Components/CV.css";
+
 export default function CV(props) {  
     
     return (
+      <body>
+      <div>
+        <br></br>
+        <iframe src="CV_LeenaPalsio.pdf"></iframe>
+      </div>
+
+      <div>
         <object
         data="https://drive.google.com/file/d/11KiZnxxEMyMhMj3Dp0MUNC74FDHNa3cx/view?usp=sharing"
         type="application/pdf"
@@ -10,5 +19,8 @@ export default function CV(props) {
         height="100%">
           <a href="https://drive.google.com/file/d/11KiZnxxEMyMhMj3Dp0MUNC74FDHNa3cx/view?usp=sharing">Avaa PDF</a>
       </object>
+      </div>
+
+      </body>
     );
 }
