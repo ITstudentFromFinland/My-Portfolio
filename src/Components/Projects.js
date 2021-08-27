18f46d771e94b79937cb7672a66b69b6b6d1a6df
Leena Palsio <leena@outlook.com>
2021-08-27 23:06:08 +0300

    added new project to project listing on projects page

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -12,6 +12,8 @@ export default function Projects(props) {
                 <li><a href="https://github.com/ITstudentFromFinland/MobiDogi">MobiDogi</a> (Android-mobiilisovellus koirien ongelmakoulutukseen, opinnäytetyö)</li>
             <br></br>
                 <li><a href="https://github.com/ITstudentFromFinland/bootcamp-finland-traveling-demo">Travel Planner</a> (Websovellus matkojen suunnitteluun ystävien kanssa, Code Bootcamp -demosovellus)</li>
+            <br></br>
+                <li><a href="https://github.com/ITstudentFromFinland/My-Portfolio">Portfolioni</a> (tämä websovellus)</li>
             </ul>
         </div>
 
